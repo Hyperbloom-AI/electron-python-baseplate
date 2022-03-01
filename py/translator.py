64d88dce0eb4755885d4c75db91d7040547e4a51
mcnamara-charles <c.w.mcnamara@vikes.csuohio.edu>
2022-03-01 00:50:53 -0500

    M Commit

--- py/translator.py
+++ py/translator.py
@@ -2,22 +2,20 @@ from fileinput import filename
 import sys
 from sys import argv
 import datetime, pandas as pd, json, os
+from random import randrange
 
 def translate(path, config):
     df = pd.read_csv(path) # path contains an absolute path to read
-    config_reader = {}
-    states_list = {}
-    ga_counties_list = {}
-    ga_counties_list
+    config_reader, states_list, ga_counties_list, banner_codes = {}
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     with open(config) as json_file:
         config_reader = json.load(json_file)
     with open(os.path.join(__location__, 'states.json')) as json_file_st:
-    # with open('C:\\Users\\Charles McNamara\\OneDrive\\Documents\\Work\\electron-poc\\py\\states.json') as json_file_st:
         states_list = list((json.load(json_file_st)).keys())
     with open(os.path.join(__location__, 'ga-counties.json')) as json_file_co:
-    #with open('C:\\Users\\Charles McNamara\\OneDrive\\Documents\\Work\\electron-poc\\py\\ga-counties.json') as json_file_co:
          ga_counties_list = json.load(json_file_co)
+    with open(os.path.join(__location__, 'banner-codes.json')) as json_file_co:
+         banner_codes = json.load(json_file_co)
 
     
     config_functions = config_reader["FUNCTIONS"]
@@ -45,9 +43,15 @@ def translate(path, config):
 
             elif(transformation["type"] == "removeValuesInSeparateColumnBasedOnThisColumn"):
                 remove_values_in_other_column_based_on_initial(df, function["columnName"], transformation["separateColumnName"], transformation["replace"], transformation["replaceType"])
+            
+            elif(transformation["type"] == "replaceValuesInSeparateColumnBasedOnThisColumn"):
+                replace_values_in_other_column_based_on_initial(df, function["columnName"], transformation["separateColumnName"], transformation["replacements"])
 
             elif(transformation["type"] == "replaceWithValueInList"):
-                replace_with_listval(df, function["columnName"], ga_counties_list)
+                if(transformation["list"] == "Georgia Counties"):
+                    replace_with_listval_county(df, function["columnName"], ga_counties_list)
+                else:
+                    replace_with_listval(df, function["columnName"], banner_codes)
             
             elif(transformation["type"] == "groupColumns"):
                 df = reorder_columns(df, transformation["columnList"])
@@ -65,6 +69,19 @@ def translate(path, config):
 
             elif(transformation["type"] == "dropColumn"):
                 df.drop(columns=function["columnName"], inplace=True)
+
+            elif(transformation["type"] == "zFill"):
+                pad_with_zeros(df, function["columnName"])
+
+            elif(transformation["type"] == "removeColumnsUntilIndex"):
+                unshift_to(df, function["columnName"], transformation["index"])
+
+            elif(transformation["type"] == "combineWithColumn"):
+                combine_columns(df, function["columnName"], transformation["secondary"])
+                df.drop(columns=transformation["secondary"], inplace=True)
+            
+            elif(transformation["type"] == "convertToEAID"):
+                getEAID(df, function["columnName"])
 
             else:
                 print("Hit default!")
@@ -93,7 +110,14 @@ def change_column_name(frame, column_name, new_col_name):
 
 def replace_values(frame, column_name, replaceArray):
     for replacement in replaceArray:
-        frame[column_name].replace([replacement["original"]], [replacement["new"]], inplace=True)
+        if(replacement["original"] == "*" and isinstance(column_name, int)):
+            frame.iloc[:, column_name] = replacement["new"]
+        elif(replacement["original"] == "*"):
+            frame[column_name] = replacement["new"]
+        elif(isinstance(column_name, int)):
+            frame.iloc[:, column_name].replace([replacement["original"]], [replacement["new"]], inplace=True)
+        else:
+            frame[column_name].replace([replacement["original"]], [replacement["new"]], inplace=True)
 
 def replace_empty_values(frame, column_name, replacement):
     frame.loc[frame[column_name].isnull(), column_name] = replacement
@@ -129,10 +153,23 @@ def remove_values_in_other_column_based_on_initial(frame, column_name, column_na
     else:
         frame.loc[frame[column_name] == value, column_name_to_change] = ""
 
+def replace_values_in_other_column_based_on_initial(frame, column_name, column_name_to_change, replaceArray):
+    for replacement in replaceArray:
+        frame.loc[frame[column_name] == replacement["value"], column_name_to_change] = replacement["replacement"]
+
 def replace_with_listval(frame, column_name, arr):
     frame[column_name] = frame[column_name].apply(replacement_lists, args=([arr]))
 
 def replacement_lists(x, a):
+    if x in list(a.keys()):
+        return a[x]
+    else:
+        return ""
+
+def replace_with_listval_county(frame, column_name, arr):
+    frame[column_name] = frame[column_name].apply(replacement_lists_county, args=([arr]))
+
+def replacement_lists_county(x, a):
     if (x.upper().replace("COUNTY", "").replace(" ", "")) in list(a.keys()):
         return a[x.upper().replace("COUNTY", "").replace(" ", "")]
     else:
@@ -142,6 +179,10 @@ def reorder_columns(frame, column_list):
     cols = list(frame.columns.values)
     try:
         index = cols.index(column_list[0])
+        for col in column_list:
+            n = cols.index(col)
+            if n < index:
+                index = n
         for col in column_list:
             cols.remove(col)
             cols.insert(index, col)
@@ -174,6 +215,42 @@ def n_2_m(x):
         return ""
     month_list = ['January', 'February', "March", 'April', 'May', 'June', 'July', 'August', 'Sepetember', 'October', 'November', 'December']
     return month_list[((int(float(x)))-1)]
+
+def pad_with_zeros(frame, column_name):
+    frame[column_name] = frame[column_name].apply(pwz)
+
+def pwz(x):
+    if(pd.isna(x) or x == ""):
+        return ""
+    else:
+        return f'"{int(x):03d}"'
+
+def unshift_to(frame, column_name, i):
+    ci = frame.columns.get_loc(column_name)
+    while(ci > i):
+        frame.drop(frame.columns[i], axis=1, inplace=True)
+        ci = frame.columns.get_loc(column_name)
+
+def combine_columns(frame, primary_column, secondary_column):
+    frame[primary_column] = frame[primary_column].astype(str) + frame[secondary_column].astype(str)
+
+def getEAID(frame, column_name):
+    frame[column_name] = frame[column_name].apply(EAID)
+
+def EAID(x):
+    nc = x.replace(",", "").replace("at 12:00 AM", "").rstrip().lstrip().split(" ")
+    month_text = nc[0]
+    day = (str(nc[1])).zfill(2)
+    year = nc[2]
+    datetime_object = datetime.datetime.strptime(month_text, "%b")
+    month = (str(datetime_object.month)).zfill(2)
+    hour = (str(randrange(13)))
+    minute = (str(randrange(61))).zfill(2)
+    second = (str(randrange(61))).zfill(2)
+    return year + month + day + hour + minute + second
+
+
+
     
 
 if __name__ == '__main__':
